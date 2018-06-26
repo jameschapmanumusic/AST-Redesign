5c6b4c0953a2ac47e0a7fcbd2f1e742cb410e789
kingmi <mike.king@umusic.com>
2018-06-26 11:46:41 +0100

    ast redesign

--- dist/js/temp.js
+++ dist/js/temp.js
@@ -3,8 +3,41 @@ function activateWiz() {
     document.getElementById("wiz").classList.toggle("show-wizard");
 }
 
-
 document.getElementById("uploads").onclick = function () { toggleUploadsDrawer() };
 function toggleUploadsDrawer() {
     document.getElementById("uploads").classList.toggle("open");
 }
+
+document.getElementById("rotate").onclick = function () { toggleRotation() };
+function toggleRotation() {
+    document.getElementById("rotate").classList.toggle("rotate");
+}
+
+// toggle active class
+function toggleActiveClass(e) {
+    var elems = document.querySelectorAll(".selected");
+    [].forEach.call(elems, function (el) {
+        el.classList.remove("selected");
+    });
+    e.target.className = "selected";
+}
+
+// Show Form
+document.getElementById("showForm").onclick = function () { displayForm() };
+function displayForm() {
+    document.getElementById("toggleForm").classList.toggle("show-form");
+}
+
+// Cancel Form
+document.getElementById("cancelForm").onclick = function () { cancelForm() };
+function cancelForm() {
+    document.getElementById("toggleForm").classList.toggle("show-form");
+}
+
+// MK - toggle row open and closed
+$(".main-table-section .table-striped tbody tr.details-row").each(function () {
+    var $thisRow = $(this);
+    $thisRow.click(function () {
+        $thisRow.toggleClass("row-open");
+    });
+});
